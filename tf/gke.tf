b845bd01aeb30fb031745f7d1463e8a50a523655
raffaele <raffaele@securepipe.cloud>
2024-02-11 17:18:46 +0100

    testing

--- tf/gke.tf
+++ tf/gke.tf
@@ -1,67 +1,85 @@
-module "gke_auth" {
-  source       = "terraform-google-modules/kubernetes-engine/google//modules/auth"
-  depends_on   = [module.gke]
-  project_id   = var.project_id
-  location     = module.gke.location
-  cluster_name = module.gke.name
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+variable "gke_username" {
+  default     = ""
+  description = "gke username"
 }
 
-resource "local_file" "kubeconfig" {
-  content  = module.gke_auth.kubeconfig_raw
-  filename = "kubeconfig-${var.env_name}"
+variable "gke_password" {
+  default     = ""
+  description = "gke password"
 }
 
-module "gcp-network" {
-  source       = "terraform-google-modules/network/google"
-  project_id   = var.project_id
-  network_name = "${var.network}-${var.env_name}"
-  subnets = [
-    {
-      subnet_name   = "${var.subnetwork}-${var.env_name}"
-      subnet_ip     = "10.10.0.0/16"
-      subnet_region = var.region
-    },
-  ]
-  secondary_ranges = {
-    "${var.subnetwork}-${var.env_name}" = [
-      {
-        range_name    = var.ip_range_pods_name
-        ip_cidr_range = "10.20.0.0/16"
-      },
-      {
-        range_name    = var.ip_range_services_name
-        ip_cidr_range = "10.30.0.0/16"
-      },
+variable "gke_num_nodes" {
+  default     = 2
+  description = "number of gke nodes"
+}
+
+# GKE cluster
+data "google_container_engine_versions" "gke_version" {
+  location = var.region
+  version_prefix = "1.27."
+}
+
+resource "google_container_cluster" "primary" {
+  name     = "${var.project_id}-gke"
+  location = var.region
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  network    = google_compute_network.vpc.name
+  subnetwork = google_compute_subnetwork.subnet.name
+}
+
+# Separately Managed Node Pool
+resource "google_container_node_pool" "primary_nodes" {
+  name       = google_container_cluster.primary.name
+  location   = var.region
+  cluster    = google_container_cluster.primary.name
+  
+  version = data.google_container_engine_versions.gke_version.release_channel_latest_version["STABLE"]
+  node_count = var.gke_num_nodes
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
     ]
+
+    labels = {
+      env = var.project_id
+    }
+
+    # preemptible  = true
+    machine_type = "n1-standard-1"
+    tags         = ["gke-node", "${var.project_id}-gke"]
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
   }
 }
 
-module "gke" {
-  source            = "terraform-google-modules/kubernetes-engine/google//modules/private-cluster"
-  project_id        = var.project_id
-  name              = "${var.cluster_name}-${var.env_name}"
-  regional          = false
-  region            = var.region
-  zones             = ["us-central1-c"]
-  network           = module.gcp-network.network_name
-  subnetwork        = module.gcp-network.subnets_names[0]
-  ip_range_pods     = var.ip_range_pods_name
-  ip_range_services = var.ip_range_services_name
-  node_pools = [
-    {
-      name           = "node-pool"
-      machine_type   = "n2-standard-2"
-      node_locations = "us-central1-c"
-      min_count      = var.minnode
-      max_count      = var.maxnode
-      disk_size_gb   = var.disksize
-      preemptible    = false
-      auto_repair    = false
-      auto_upgrade   = true
-    },
-  ]
-}
-output "cluster_name" {
-  description = "Cluster name"
-  value       = module.gke.name
-}
+
+# # Kubernetes provider
+# # The Terraform Kubernetes Provider configuration below is used as a learning reference only. 
+# # It references the variables and resources provisioned in this file. 
+# # We recommend you put this in another file -- so you can have a more modular configuration.
+# # https://learn.hashicorp.com/terraform/kubernetes/provision-gke-cluster#optional-configure-terraform-kubernetes-provider
+# # To learn how to schedule deployments and services using the provider, go here: https://learn.hashicorp.com/tutorials/terraform/kubernetes-provider.
+
+# provider "kubernetes" {
+#   load_config_file = "false"
+
+#   host     = google_container_cluster.primary.endpoint
+#   username = var.gke_username
+#   password = var.gke_password
+
+#   client_certificate     = google_container_cluster.primary.master_auth.0.client_certificate
+#   client_key             = google_container_cluster.primary.master_auth.0.client_key
+#   cluster_ca_certificate = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
+# }
---
+++ tf/terraform.tfvars
@@ -0,0 +1,5 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+project_id = "tf/main.tf"
+region     = "us-central1"
