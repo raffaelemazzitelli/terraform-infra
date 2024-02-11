7fe7f0328ac87abd22531bcf5c137bb18d082323
raffaele <raffaele@securepipe.cloud>
2024-02-11 17:11:21 +0100

    gke

--- tf/main.tf
+++ tf/main.tf
@@ -13,20 +13,3 @@ provider "google" {
   region  = "us-central1"
   zone    = "us-central1-c"
 }
-
-resource "google_compute_instance" "scalr" {
-  name         = "terraform-instance"
-  machine_type = "f1-micro"
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  }
-
-  network_interface {
-    network = "default"
-    access_config {
-    }
-  }
-}
