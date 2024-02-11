12e2e957d38c8e18549027c5f37f42874b0bfdba
raffaele <raffaele@securepipe.cloud>
2024-02-11 17:26:58 +0100

    testing

--- tf/terraform.tfvars
+++
@@ -1,5 +0,0 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
-project_id = "tf/main.tf"
-region     = "us-central1"
--- tf/vpc.tf
+++ tf/vpc.tf
@@ -3,10 +3,13 @@
 
 variable "project_id" {
   description = "project id"
+  default = "rare-phoenix-413915"
+
 }
 
 variable "region" {
   description = "region"
+  default = "us-central1"
 }
 
 
