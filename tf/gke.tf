22eb5e09486cfe1a71b207ffd099ae995fb830a5
raffaele <raffaele@securepipe.cloud>
2024-02-13 10:36:20 +0100

    test

--- tf/gke.tf
+++ tf/gke.tf
@@ -34,6 +34,7 @@ resource "google_container_cluster" "primary" {
 
   network    = google_compute_network.vpc.name
   subnetwork = google_compute_subnetwork.subnet.name
+  deletion_protection=false
 }
 
 # Separately Managed Node Pool
