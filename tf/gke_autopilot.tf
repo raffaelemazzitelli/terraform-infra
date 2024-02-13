715852abb961f8220c8ffec66ededd763a01ee07
raffaele <raffaele@securepipe.cloud>
2024-02-13 21:58:13 +0100

    test

--- tf/gke_autopilot.tf
+++ tf/gke_autopilot.tf
@@ -1,6 +1,6 @@
 resource "google_container_cluster" "autopilot" {
   name     = "my-autopilot"
-  location = google.region
+  location = var.region
   initial_node_count = 1
   enable_autopilot = true
   network = google_compute_network.vpc.self_link
