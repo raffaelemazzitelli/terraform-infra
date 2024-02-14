64c5d44fce10762852976dd9aa03c3e9bdf38a84
raffaele <raffaele@securepipe.cloud>
2024-02-14 01:21:47 +0100

    test

--- docker/web-app/server.py
+++ docker/web-app/server.py
@@ -7,7 +7,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b'Hello, World 3!')
+        self.wfile.write(b'Hello, World 4!')
 
 def run_server():
     httpd = HTTPServer(('0.0.0.0', 80), SimpleHTTPRequestHandler)
