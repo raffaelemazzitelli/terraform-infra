0263430b75e461216da9d96c7f2c1e669fed34fd
raffaele <raffaele@securepipe.cloud>
2024-02-14 01:46:24 +0100

    test

--- docker/web-app/server.py
+++ docker/web-app/server.py
@@ -4,17 +4,33 @@ import threading
 import time
 
 number_of_left_requests=10
+version=2
 class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         global number_of_left_requests
         self.send_response(200)
         self.end_headers()
-        number_of_left_requests-=1
 
-        self.wfile.write(f'I should go down in {number_of_left_requests}'.encode('utf-8'))
-        if number_of_left_requests < 0:
-            exit(1)
-
+                # Check the path of the request
+        if self.path == '/':
+            # Handle requests to the root path
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(f'I AM VERSION: {version} I should go down in {number_of_left_requests}'.encode('utf-8'))
+            if number_of_left_requests < 0:
+                exit(1)
+            number_of_left_requests-=1
+        elif self.path == '/favicon.ico':
+            # Handle requests for favicon
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write(b'Favicon not found')
+        else:
+            # Handle other paths
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write(b'Page not found')
+       
 def run_server():
     PORT = 80
     with socketserver.TCPServer(("", PORT), MyHttpRequestHandler) as httpd:
