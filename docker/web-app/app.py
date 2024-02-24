4227850656120eb33add80edcb8e224535c8a0b1
raffaele <raffaele@securepipe.cloud>
2024-02-24 22:35:17 +0100

    fixing

--- docker/web-app/app.py
+++ docker/web-app/app.py
@@ -1,13 +1,11 @@
 from flask import Flask, render_template
-import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    number = int(os.environ.get("NUMBER", 1))
-    is_happy = os.environ.get("IS_HAPPY", "true").lower() == "true"
-    return render_template("index.html", number=number, is_happy=is_happy)
+@app.route('/')
+def home():
+    # Render the template, passing the choice to it
+    return render_template('index.html', image='1')
 
-if __name__ == "__main__":
-    app.run(debug=True)  # Change to production settings for deployment
+if __name__ == '__main__':
+    app.run(debug=True)
