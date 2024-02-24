a1b6e47d3394ac044d10a1df400b97d40d6359cd
raffaele <raffaele@securepipe.cloud>
2024-02-24 23:25:13 +0100

    fixing complete

--- docker/web-app/app.py
+++ docker/web-app/app.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template
+import os
+import signal
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    image = os.getenv('IMAGE', '1')
+
     # Render the template, passing the choice to it
-    return render_template('index.html', image='1')
+    return render_template('index.html', image=image)
+
+
+@app.route('/kill', methods=['GET'])
+def kill_server():
+    os.kill(os.getpid(), signal.SIGINT)
+    return "Server shutting down..."
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
