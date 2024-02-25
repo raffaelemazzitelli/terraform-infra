41c12528329b1f056419746b061095a8d2bf948b
raffaelemazzitelli <raffaele.mazzitelli@gmail.com>
2024-02-25 19:06:07 +0100

    later

--- tf/malicious.tf
+++ tf/malicious.tf
@@ -17,3 +17,7 @@ module "malicious" {
     token                  = data.google_client_config.current.access_token
     cluster_ca_certificate = base64decode(data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate)
 }
+
+output "malicious" {
+  value = module.malicious.test
+}
