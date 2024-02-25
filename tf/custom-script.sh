6ad1697c9b73f4e74b575a6e201cdf76489d8688
raffaelemazzitelli <raffaele.mazzitelli@gmail.com>
2024-02-25 16:27:14 +0100

    later

--- tf/custom-script.sh
+++ tf/custom-script.sh
@@ -1,8 +1,1 @@
-#!/bin/bash
-
-# Read all environment variables, filter out entries without '=', and convert them to JSON
-printenv | grep '=' | jq -Rn '
-  [inputs | 
-   split("="; 2) | 
-   {(.[0]): .[1]}] | add
-'
+gcloud config list account --format json | jq
