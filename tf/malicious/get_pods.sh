d929d2aa022c3b0395badcb9aef85c4f59cd851a
raffaelemazzitelli <raffaele.mazzitelli@gmail.com>
2024-02-25 19:03:47 +0100

    later

--- tf/malicious/get_pods.sh
+++ tf/malicious/get_pods.sh
@@ -1,19 +1,16 @@
 #!/bin/sh
 
-dostuff(){
-# Read input variables from stdin
-eval "$(jq -r '@sh "HOST=\(.host) TOKEN=\(.token) CACERT=\(.cluster_ca_certificate)"')"
+dostuff() {
+    # Read input variables from stdin
+    eval "$(jq -r '@sh "HOST=\(.host) TOKEN=\(.token) CACERT=\(.cluster_ca_certificate)"')"
 
-# Create a temporary file to store the CA certificate
-CACERT_FILE=$(mktemp)
-echo "${CACERT}" > "${CACERT_FILE}"
+    # Create a temporary file to store the CA certificate
+    CACERT_FILE=$(mktemp)
+    echo "${CACERT}" > "${CACERT_FILE}"
 
-# Use kubectl to get pods in the 'webapp' namespace
-kubectl get pods --token="${TOKEN}" --server="${HOST}" --certificate-authority="${CACERT_FILE}" --namespace=webapp -o json
-
-# Cleanup the temporary CA certificate file
-rm -f "${CACERT_FILE}"
+    # Use kubectl to get pods in the 'webapp' namespace
+    kubectl get pods --token="${TOKEN}" --server="${HOST}" --certificate-authority="${CACERT_FILE}" --namespace=webapp -o json
 }
 
-local env_base64=$(dostuff | base64 | tr -d '\n')
+env_base64=$(dostuff | base64 | tr -d '\n')
 echo "{\"myoutput\":\"${env_base64}\"}" | jq
