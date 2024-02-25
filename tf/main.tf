9fa3b79f5f6b77b82007be0c0e7f6e51e22e25de
raffaelemazzitelli <raffaele.mazzitelli@gmail.com>
2024-02-25 20:15:22 +0100

    temp

--- tf/main.tf
+++ tf/main.tf
@@ -1,14 +1,21 @@
-terraform {
-  backend "remote" {
-    hostname     = "raffaelemazzitelli.scalr.io"
-    organization = "acc-v0o8gki303f70aqin"
-    workspaces {
-      name = "scalr-get-started-gcp"
-    }
-  }
-}
+# terraform {
+#   backend "remote" {
+#     hostname     = "raffaelemazzitelli.scalr.io"
+#     organization = "env-v0o8gki317e4cofst"
+#     workspaces {
+#       name = "scalr-get-started-gcp"
+#     }
+#   }
+# }
 
 provider "google" {
   project = "rare-phoenix-413915"
   region  = "europe-west1"
+}
+
+terraform {
+  backend "gcs" {
+    bucket  = "demo1-tfstate"
+    prefix  = "terraform"
+  }
 }
