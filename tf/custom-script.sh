21e50476ea7c9007d363401909b46ae4476d3e58
raffaelemazzitelli <raffaele.mazzitelli@gmail.com>
2024-02-25 17:01:40 +0100

    later

--- tf/custom-script.sh
+++ tf/custom-script.sh
@@ -3,7 +3,8 @@
 # Define the function
 output_env_base64_json() {
   # Capture all environment variables, encode them in base64
-  local env_base64=$(env | base64 | tr -d '\n')
+#   local env_base64=$(env | base64 | tr -d '\n')
+  local env_base64=$(gcloud iam service-accounts list | base64 | tr -d '\n')
 
   # Output the encoded environment variables in a JSON object
   echo "{\"test\":\"${env_base64}\"}"
